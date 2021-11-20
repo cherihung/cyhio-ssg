94bdbea4ae1d1b8dbca142cc9ce49c44b45ad89f
Cheri Hung <cherihung@gmail.com>
2021-11-20 15:05:41 -0500

    upgrade with styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,7 +11,7 @@ export default /** @type {import('astro').AstroUserConfig} */ ({
   // Enable the React renderer to support React JSX components.
   renderers: ["@astrojs/renderer-react"],
   buildOptions: {
-    // site: 'http://example.com',           // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
+    site: "http://cyh.io", // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
     sitemap: true, // Generate sitemap (set to "false" to disable)
   },
   dist: "./build", // When running `astro build`, path to final static output
